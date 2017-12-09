76aaf4871eafb6475c44b0059ac87436e2045930
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-12-09 14:31:43 +0100

    new file:   rbdns/add-packages.sh new file:   rbdns/config/etc@cron.d@backuphome new file:   rbdns/config/etc@cron.d@raspboot new file:   rbdns/mod-files.sh

---
+++ rbdns/add-packages.sh
@@ -0,0 +1,28 @@
+
+install_package()
+{
+  # See if packages are installed and install them.
+  package=$1
+  echo "*********************************************************"
+  echo "* Requesting $package"
+  status=$(dpkg-query -W -f='${Status} ${Version}\n' $package 2>/dev/null | wc -l)
+  if [ "$status" -eq 0 ]; then
+    echo "* Installing $package"
+    echo "*********************************************************"
+    sudo apt-get -yuV install $package
+  else
+    echo "* Already installed !!!"
+    echo "*********************************************************"
+  fi
+}
+
+echo "Installing additional packages..."
+install_package "graphviz"
+install_package "php-cgi"
+install_package "php-common"
+install_package "php"
+install_package "php-sqlite3"
+install_package "nmap"
+install_package "tcpdump"
+install_package "traceroute"
+install_package "tshark"
